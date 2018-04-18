7f2d97d658898a64ca8c4cec1ec07f8159c2d399
makarimachmad <makarimachmad@gmail.com>
2018-04-19 02:19:38 +0700

    Java: -menambah pakcage slider -cek News.class
    XML:
    - activity_imageslider
    - fragment_slider_item

--- app/src/main/java/com/pilgubjateng/battistrada/pilgubjateng/News.java
+++ app/src/main/java/com/pilgubjateng/battistrada/pilgubjateng/News.java
@@ -3,14 +3,27 @@ package com.pilgubjateng.battistrada.pilgubjateng;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
+import java.util.List;
+
+
+import com.pilgubjateng.battistrada.pilgubjateng.slider.FragmentSlider;
+import com.pilgubjateng.battistrada.pilgubjateng.slider.SliderIndicator;
+import com.pilgubjateng.battistrada.pilgubjateng.slider.SliderPagerAdapter;
+import com.pilgubjateng.battistrada.pilgubjateng.slider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by mhr on 25/03/2018.
@@ -22,6 +35,11 @@ public class News extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private ArrayList<ModelNews> mSportsData;
     private NewsAdapter mAdapter;
+
+    private SliderPagerAdapter SAdapter;
+    private SliderIndicator mIndicator;
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
 
     ImageView newsImage;
 
@@ -37,6 +55,11 @@ public class News extends AppCompatActivity {
 
         //kondisi jika akan melakukan back act
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        sliderView = (SliderView) findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) findViewById(R.id.pagesContainer);
+        setupSlider();
 
         /*
 
@@ -69,6 +92,23 @@ public class News extends AppCompatActivity {
         initializeData();
 
     }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance("http://www.kpu.go.id/./application/modules/post/images/c36f54c1a0c399851b90938249e4d890.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.kpu.go.id/./application/modules/post/images/2b75b28bbec51ce56d16d4f4791c7690.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.kpu.go.id/./application/modules/post/images/6a6d99a7806cbaf29407526b0a6f09c1.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.kpu.go.id/./application/modules/post/images/f00d320c4bebc88822e91472d9cfe1c8.jpg"));
+        fragments.add(FragmentSlider.newInstance("http://www.kpu.go.id/./application/modules/post/images/fd92b9ce414f38365896b95c055a6ed9.JPG"));
+
+        SAdapter = new SliderPagerAdapter(getSupportFragmentManager(), fragments);
+        sliderView.setAdapter(SAdapter);
+        mIndicator = new SliderIndicator(this, mLinearLayout, sliderView, R.drawable.indicator_circle2);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
+    }
+
 
     /**
      * Method for initializing the sports data from resources.
